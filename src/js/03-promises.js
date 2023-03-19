06e88ac55a12f9792b418a74150793a55f403ab8
Ivan-Tishchenko <114497011+Ivan-Tishchenko@users.noreply.github.com>
2023-03-19 11:56:52 +0200

    promises

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,48 @@
+import Notiflix from "notiflix";
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  const promis =new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    if (shouldResolve) {
+      resolve({
+        position,
+        delay,
+      })
+    } else {
+      reject({
+        position,
+        delay,
+      })
+    }
+  });
+  return promis;
+}
+
+const form = document.querySelector("form");
+let interval = null;
+
+form.addEventListener("submit", startGeneratPromis);
+
+function startGeneratPromis(e) {
+  e.preventDefault(); 
+
+  let inputDelay = Number.parseFloat(document.querySelector("[name=delay]").value);
+  let inputStep = Number.parseFloat(document.querySelector("[name=step]").value);
+  let inputAmount = Number.parseFloat(document.querySelector("[name=amount]").value);
+  let i = 1;
+
+  interval = setInterval(() => {
+    createPromise(i, inputDelay).then(({ position, delay }) => {
+      console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+    }).catch(({ position, delay }) => {
+      console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+    });
+    i += 1;
+    inputDelay += inputStep;
+
+    if (i > inputAmount) {
+      clearInterval(interval);
+    }
+
+  }, inputStep)
 }
